71b26d0138c286acf3239688bc9cfc1b6a2bdc70
Tenu <tenu2022@gmail.com>
2022-11-01 10:46:25 +0800

    modify

--- main2.py
+++ main2.py
@@ -4,15 +4,15 @@ import tkinter as tk
 
 class ColorCanvas(tk.Canvas):
     def __init__(self,parent,rec_color,**kwargs):
-        width = kwargs['width']
-        height = kwargs['height']
+        self.width = kwargs['width']
+        self.height = kwargs['height']
         super().__init__(parent,**kwargs)
         self.rec_color = rec_color
         self.__state = False
-        space = width/7
-        rec_width = width - 2*space
-        rec_height = width - 2*space
-        self.create_rectangle(space,space,width-space,height-space,fill=self.rec_color)
+        self.space = self.width/7
+        rec_width = self.width - 2*self.space
+        rec_height = self.width - 2*self.space
+        self.create_rectangle(self.space,self.space,self.width-self.space,self.height-self.space,fill=self.rec_color)
 
     @property
     def state(self):
@@ -20,6 +20,11 @@ class ColorCanvas(tk.Canvas):
     @state.setter
     def state(self,s):
         self.__state = s
+        self.delete()
+        self.create_rectangle(self.space,self.space,self.width-self.space,self.height-self.space,fill=self.rec_color)
+        if self.__state == True:
+            #多加小圓點
+            print("多加小圓點")
 
 class Window(tk.Tk):
     def __init__(self):
