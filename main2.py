9d21c9f5c3337910afe3015512547a938dfda52e
Tenu <tenu2022@gmail.com>
2022-11-01 11:17:40 +0800

    modify

--- main2.py
+++ main2.py
@@ -8,6 +8,7 @@ class ColorCanvas(tk.Canvas):
     ON = True
     OFF = False
 
+    #建立實體裡的attribute
     def __init__(self,parent,rec_color,**kwargs):
         self.width = kwargs['width']
         self.height = kwargs['height']
@@ -15,10 +16,9 @@ class ColorCanvas(tk.Canvas):
         self.rec_color = rec_color
         self.__state = ColorCanvas.OFF
         self.space = self.width/7
-        rec_width = self.width - 2*self.space
-        rec_height = self.width - 2*self.space
         self.create_rectangle(self.space,self.space,self.width-self.space,self.height-self.space,fill=self.rec_color)
-
+    
+    #建立實體裡的property
     @property
     def state(self):
         return self.__state
@@ -30,6 +30,18 @@ class ColorCanvas(tk.Canvas):
         if self.__state == True:
             #多加小圓點
             print("多加小圓點")
+            rec_width = self.width - 2*self.space
+            rec_height = self.width - 2*self.space
+
+            cir_width = rec_width / 5
+            cir_height = rec_height / 5
+            cir_start_x = self.space + cir_width / 2
+            cir_end_x = cir_start_x + cir_width
+            cir_start_y = self.space + rec_height * 5 / 7
+            cir_end_y  =  cir_start_y + cir_height
+            self.create_oval(cir_start_x,cir_start_y,cir_end_x,cir_end_y,fill='white',outline='white')
+
+
 
 class Window(tk.Tk):
     def __init__(self):
@@ -38,13 +50,14 @@ class Window(tk.Tk):
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
         red.state = ColorCanvas.ON
-        print(f"red的狀態:{red.state}")
 
         green =ColorCanvas(self,"green",width=100,height=100)
         green.grid(row=0, column=1)
+        green.state = ColorCanvas.ON
 
         blue =ColorCanvas(self,"blue",width=100,height=100)
         blue.grid(row=0, column=2)
+        blue.state = ColorCanvas.ON
 
     def mouse_click(self,event):
         print(event.__dict__)
