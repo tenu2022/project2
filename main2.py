346fa3c103d7c39ee6c89530219c8a7282d024dc
Tenu <tenu2022@gmail.com>
2022-11-01 10:00:03 +0800

    modify

--- main2.py
+++ main2.py
@@ -3,21 +3,22 @@ import tkinter as tk
 
 
 class ColorCanvas(tk.Canvas):
-    def __init__(self,parent,**kwargs):
+    def __init__(self,parent,rec_color,**kwargs):
+        width = kwargs['width']
+        height = kwargs['height']
         super().__init__(parent,**kwargs)
-
+        self.rec_color = rec_color
+        space = 10
+        rec_width = width - 2*space
+        rec_height = width - 2*space
+        self.create_rectangle(space,space,width-space,height-space,fill=self.rec_color)
 
 
 
 class Window(tk.Tk):
     def __init__(self):
         super().__init__()
-        red =ColorCanvas(self,width=70,height=70,relief='ridge')
-        red.create_rectangle(10,10,60,60,fill="red")
-        x = 70*1/5
-        y = 70*4/6
-        width = 10
-        red.create_oval(x,y,x+10,y+10,fill="white",outline="white")
+        red =ColorCanvas(self,"red",width=70,height=70)
         red.bind('<ButtonRelease-1>',self.mouse_click)
         red.grid(row=0, column=0)
 
